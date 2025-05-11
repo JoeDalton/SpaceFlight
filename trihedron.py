df1cb5a5e69872444285dccdaa7b010307aa4d17
glavabre <guilhem.lavabre@airseas.com>
2025-05-11 18:32:08 +0200

    much simpler trihedron class

--- trihedron.py
+++ trihedron.py
@@ -1,53 +1,12 @@
-import numpy as np
 from direct.showbase.ShowBase import ShowBase
-from panda3d.core import (
-    Geom,
-    GeomLines,
-    GeomNode,
-    GeomVertexData,
-    GeomVertexFormat,
-    GeomVertexWriter,
-    NodePath,
-)
 
 
 class Trihedron:
-    """
-    A class to display a trihedron, based on a parent Node
-
-    It can be used either for debug or for a clearer visualization.
-    """
-
-    def __init__(self, app: ShowBase, parent):
-        self.app = app
-        geom = make_axes()
-        node = GeomNode("axis")
-        node.addGeom(geom)
-        self.object = NodePath(node)
-        self.object.setScale(10, 10, 10)
-        self.object.reparentTo(parent)
-
-
-def make_axes():
-    """
-    Defines the render of a trihedron
-    """
-    vformat = GeomVertexFormat.get_v3c4()
-    vdata = GeomVertexData("vdata", vformat, Geom.UHStatic)
-    vdata.uncleanSetNumRows(6)
-
-    vertex = GeomVertexWriter(vdata, "vertex")
-    color = GeomVertexWriter(vdata, "color")
-
-    for x, y, z in np.eye(3):
-        vertex.addData3(0, 0, 0)
-        color.addData4(x, y, z, 1)
-        vertex.addData3(x, y, z)
-        color.addData4(x, y, z, 1)
-
-    prim = GeomLines(Geom.UHStatic)
-    prim.addNextVertices(6)
-
-    geom = Geom(vdata)
-    geom.addPrimitive(prim)
-    return geom
+    def __init__(self, app: ShowBase, parent, scale: int = 10):
+        axis = app.loader.loadModel("zup-axis")
+        # make sure it will be drawn above all other elements
+        axis.setDepthTest(False)
+        axis.setBin("fixed",0)
+        axis.setScale(scale)
+        axis.reparentTo(parent)
+        axis.setPos(0, 0, 0)
