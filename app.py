6fb52ce8313097b1161be7b7f8aa6557c3a35449
Joe Dalton <guilhem.lavabre@laposte.net>
2025-12-29 19:18:12 +0100

    Added bot ships

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ from direct.showbase import Audio3DManager
 
 import numpy as np
 
-from hud import HUD
-from cockpit_view import CockpitView
+from hud import HUD, TargetHUD
 from asteroid_field import AsteroidField
 from player import Player
 from integrator import Integrator
 from skybox import Skybox
 from trihedron import Trihedron
 from lighting import Lighting
+from bot import Bot
 
 # load_prc_file_data("", """
 #     gl-version 3 2
@@ -36,9 +36,9 @@ class MyApp(ShowBase):
         """
         self.audio3d = Audio3DManager.Audio3DManager(self.sfxManagerList[0], self.camera)
         # music = self.loader.loadMusic("sounds/music_Koyaanisqatsi.mp3")
-        music = self.loader.loadMusic("sounds/music_westworld.mp3")
-        music.setLoop(True)
-        music.setVolume(0.8)
+        # music = self.loader.loadMusic("sounds/music_westworld.mp3")
+        # music.setLoop(True)
+        # music.setVolume(0.8)
         
 
         """
@@ -66,8 +66,8 @@ class MyApp(ShowBase):
         
         # Asteroid field
         self.static_asteroid_field = AsteroidField(self, n_asteroids=2000, field_size=15000, is_moving=False)
-        self.big_static_asteroid_field = AsteroidField(self, n_asteroids=20, scale_factor=10, field_size=15000, is_moving=False)
-        self.rotating_asteroid_field = AsteroidField(self, n_asteroids=1000, field_size=15000)
+        self.big_rotating_asteroid_field = AsteroidField(self, n_asteroids=20, scale_factor=10, field_size=15000, is_moving=True)
+        self.rotating_asteroid_field = AsteroidField(self, n_asteroids=500, field_size=15000)
 
         # Drydock
         self.drydock = self.loader.load_model("models/star_trek_space_drydock/scene.gltf")
@@ -85,8 +85,23 @@ class MyApp(ShowBase):
         """
         Initialize player and ship        
         """
-        self.player = Player(self, ship_name="a-wing", ini_position=np.array([0, -9000, 0]), input_system="keyboard")
+        self.player = Player(self, ship_name="a-wing", ini_position=np.array([0, -300, 0]), input_system="keyboard")
         # self.player = Player(self, ship_name="tie-fighter")
+
+        """
+        Initialize dummy bot
+        """
+        self.bot = Bot(app=self, ship_name="tie-fighter", ini_position=np.array([0, 0, 0]))
+        self.bot.set_mode("idle")
+        # wp_distance = 60
+        # bot_waypoints = [
+        #     np.array([0, wp_distance, 0]),
+        #     np.array([0, wp_distance, wp_distance]),
+        #     np.array([0, 0, wp_distance]),
+        #     np.array([wp_distance, 0, wp_distance]),
+        #     np.array([wp_distance, 0, 0]),
+        # ]
+        # self.bot.initialize_waypoints(waypoints=bot_waypoints)
         """
         Debug options
         """
@@ -94,18 +109,27 @@ class MyApp(ShowBase):
         # self.toggle_wireframe()
         self.hud = HUD(self)
         # trihedron = Trihedron(app = self, parent=self.player.ship.node, scale = 1)
+        trihedron = Trihedron(app = self, parent=self.bot.ship.node, scale = 1)
+
+        """
+        Add target HUD
+        """
+        target_hud = TargetHUD(app=self)
+        target_hud.set_target(target = self.bot.ship)
 
         """
         Initialize all tasks in the correct order
         """
         self.integrator.initialize_tasks() # Must come before all physics
         self.player.initialize_move()
+        self.bot.initialize_move()
         self.rotating_asteroid_field.initialize_move()
+        self.big_rotating_asteroid_field.initialize_move()
 
         """
         Launch music
         """
-        music.play()
+        # music.play()
 
     def collision_task(self, task):
         self.traverser.traverse(self.render)
