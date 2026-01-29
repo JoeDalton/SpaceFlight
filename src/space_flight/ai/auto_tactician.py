ec89445f101fa83653fb115f4a80694332933c60
Joe Dalton <guilhem.lavabre@laposte.net>
2026-01-29 18:38:06 +0100

    First attempt at an autotactician

--- src/space_flight/ai/auto_tactician.py
+++ src/space_flight/ai/auto_tactician.py
@@ -1,8 +1,18 @@
 import logging
+from typing import List
 
-from space_flight import DEBUG_DELETION
+import numpy as np
+
+from space_flight import DEBUG_DELETION, DISTANCE_TOLERANCE_M
+from space_flight.utils import rotate_single_vector
 
 LOGGER = logging.getLogger()
+
+ALIGNMENT_WEIGHT = 1.0
+RELATIVE_VELOCITY_WEIGHT = 0.0
+REFERENCE_DISTANCE_M = 500
+REFERENCE_VELOCITY_MPS = 1000
+SCORE_THRESHOLD_FOR_ACTION = 0.1
 
 
 class AutoTactician:
@@ -32,15 +42,15 @@ class AutoTactician:
         self.flee_weight = flee_weight
         self.patrol_weight = patrol_weight
 
-    def think(self):
+    def think(self) -> List[dict]:
         """
         Example of result
 
         behaviours = [
             {
-                "action": "flee_from_target",
-                "target": <some asteroid that's too close>,
-                "weight": 10,
+                "action": "chase_target",
+                "target": <a vulnerable enemy>, # Or a leader to follow
+                "weight": 1,
             },
             {
                 "action": "evade_target",
@@ -48,12 +58,12 @@ class AutoTactician:
                 "weight": 5,
             },
             {
-                "action": "follow_waypoints", # Only one of those please
-                "weight": 1,
+                "action": "flee_from_target",
+                "target": <some asteroid that's too close>,
+                "weight": 10,
             },
             {
-                "action": "chase_target",
-                "target": <a vulnerable enemy>, # Or a leader to follow
+                "action": "follow_waypoints", # Only one of those please
                 "weight": 1,
             },
         ]
@@ -67,7 +77,125 @@ class AutoTactician:
         Or use the collision system of panda3d ?
         Aaaaaaaaaaaaaaaaaaah, paniiiiiiic !!!
 
+        TODO: closing velocity does not seem very interesting, retrospectively. Is it?
+
         """
+        my_thoughts = [
+            {
+                "action": "follow_waypoints",
+                "weight": self.patrol_weight,
+            },
+        ]
+
+        my_actor_index = self.app.interactions.get_actor_index(self.ship)
+        directions = self.app.interactions.directions[my_actor_index, :, :]
+        distances = self.app.interactions.distances[my_actor_index, :]
+        # rel_velocities = self.app.interactions.rel_velocities[my_actor_index, :, :]
+
+        # Find if actors are hostile
+        # TODO: create an ad-hoc interaction matrix instead
+        hostile_mask = distances > DISTANCE_TOLERANCE_M
+
+        # Find if actors are forward or behind
+        ship_quat = np.quaternion(*self.ship.orientation)
+        ship_forward = rotate_single_vector(ship_quat, np.array([0.0, 1.0, 0.0]))
+        cos_alignments = np.dot(directions, ship_forward)
+        projection_signs = np.sign(cos_alignments)
+        forward_mask = projection_signs >= 0
+        behind_mask = projection_signs < 0
+
+        # Compute alignment score:
+        # The more aligned and the closer the target the more interesting it is
+        with np.errstate(divide="ignore", invalid="ignore"):
+            alignment_score = cos_alignments / distances * REFERENCE_DISTANCE_M
+
+        # Compute chasing behaviour
+        chasing_scores = (
+            hostile_mask
+            * forward_mask
+            * (
+                alignment_score
+                * ALIGNMENT_WEIGHT
+                # + closing_velocity_score * RELATIVE_VELOCITY_WEIGHT
+            )
+        )
+
+        # Only one: Cheaper !
+        # try:
+        #     chasing_idx = np.nanargmax(chasing_scores)
+        #     if chasing_scores[chasing_idx] > SCORE_THRESHOLD_FOR_ACTION:
+        #         my_thoughts.append(
+        #             {
+        #                 "action": "chase_target",
+        #                 "target": self.app.interactions.actors[chasing_idx],
+        #                 "weight": self.chase_weight,
+        #             },
+        #         )
+        # except ValueError:
+        #     pass
+
+        # Everyone: more organic ?
+        for chasing_idx in range(self.app.interactions.n_actors):
+            if chasing_scores[chasing_idx] > SCORE_THRESHOLD_FOR_ACTION:
+                my_thoughts.append(
+                    {
+                        "action": "chase_target",
+                        "target": self.app.interactions.actors[chasing_idx],
+                        "weight": self.chase_weight * chasing_scores[chasing_idx],
+                    },
+                )
+
+        # Compute evading scores
+        evading_scores = (
+            hostile_mask
+            * behind_mask
+            * (
+                -alignment_score  # negative because they are behind
+                * ALIGNMENT_WEIGHT
+                # + closing_velocity_score * RELATIVE_VELOCITY_WEIGHT
+            )
+        )
+        # Only one: Cheaper !
+        # try:
+        #     evading_idx = np.nanargmax(evading_scores)
+        #     if evading_scores[evading_idx] > SCORE_THRESHOLD_FOR_ACTION:
+        #         my_thoughts.append(
+        #             {
+        #                 "action": "evade_target",
+        #                 "target": self.app.interactions.actors[evading_idx],
+        #                 "weight": self.chase_weight,
+        #             },
+        #         )
+        # except ValueError:
+        #     pass
+
+        # Everyone : more organic ?
+        for evading_idx in range(self.app.interactions.n_actors):
+            if evading_scores[evading_idx] > SCORE_THRESHOLD_FOR_ACTION:
+                my_thoughts.append(
+                    {
+                        "action": "evade_target",
+                        "target": self.app.interactions.actors[evading_idx],
+                        "weight": self.chase_weight * evading_scores[evading_idx],
+                    },
+                )
+
+        # my_thoughts = self.placeholder_think()
+        # DEBUG
+        if self.ship.parent.name == "lead_2":
+            pass
+        return my_thoughts
+
+    def clean(self):
+        self.ship = None
+        if DEBUG_DELETION:
+            LOGGER.info("Cleaned autotactician")
+
+    def __del__(self):
+        if DEBUG_DELETION:
+            LOGGER.info("Deleted autotactician")
+
+    def placeholder_think(self):
         if self.ship.parent.name == "prey":
             my_thoughts = [
                 {
@@ -93,12 +221,3 @@ class AutoTactician:
             ]
 
         return my_thoughts
-
-    def clean(self):
-        self.ship = None
-        if DEBUG_DELETION:
-            LOGGER.info("Cleaned autotactician")
-
-    def __del__(self):
-        if DEBUG_DELETION:
-            LOGGER.info("Deleted autotactician")
