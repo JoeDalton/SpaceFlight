42b4fa796c1f383da71c142896e68c4f3c9d1732
Joe Dalton <guilhem.lavabre@laposte.net>
2026-02-05 15:14:02 +0100

    Added scape_goat personality (e.g. for unarmed transports)

--- src/space_flight/ai/__init__.py
+++ src/space_flight/ai/__init__.py
@@ -75,3 +75,53 @@ class Personality:
             "minimum_throttle": 0.05,
         },
     }
+
+    # Doesn't defends itself nor attacks
+    SCAPEGOAT = {
+        "tactician": {
+            "min_fighting_shape": 0.0,
+            "min_engagement_score": 2.0,
+            "primary_target_engagement_multiplier": 5.0,
+            "max_threat_score": 2.0,
+            "hunter_cutoff_distance": 1300.0,
+            "hunter_angular_focus": 0.7,
+            "prey_cutoff_distance": 1300.0,
+            "prey_angular_focus": 1.0,
+            "commitment_times": {
+                Intent.ENGAGE: 3.0,
+                Intent.EVADE: 1.0,
+                Intent.DISENGAGE: 5.0,
+                Intent.REGROUP: 3.0,
+                Intent.PATROL: 3.0,
+                Intent.IDLE: 0.1,
+            },
+        },
+        "navigator": {
+            "fire": {
+                "minimimum_window_duration_s": 0.5,
+                "maximum_distance_m": 600,
+                "maximum_angle_rad": np.deg2rad(5),
+                "minimum_cos_angle": np.cos(np.deg2rad(5)),
+            },
+            "attack": {
+                "lead_time_s": 0.5,
+                "maximum_duration_s": 5.0,
+                "minimum_cos_angle": np.cos(np.deg2rad(20)),
+                "maximum_distance_m": 800,
+            },
+            "intercept": {
+                "lead_time_s": 1.5,
+                "maximum_duration_s": 10.0,
+                "minimum_cos_angle": np.cos(np.deg2rad(30)),
+            },
+            "extend": {
+                "minimum_duration_s": 0.5,
+            },
+            "reposition": {"minimum_time_to_overshoot_s": 1.5},
+        },
+        "pilot": {
+            "angle_throttle_exponent": 0.5,
+            "distance_throttle_exponent": 1.1,
+            "minimum_throttle": 0.0,
+        },
+    }
