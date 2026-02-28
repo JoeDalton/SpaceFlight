379b2fa2cdca582ec3b7b775fcac0a28aee73fb1
Joe Dalton <guilhem.lavabre@laposte.net>
2026-03-01 00:12:37 +0100

    Better SpeedDustCloud

--- src/space_flight/fx/speed_dust_cloud.py
+++ src/space_flight/fx/speed_dust_cloud.py
@@ -8,14 +8,22 @@ from panda3d.core import CardMaker, NodePath, TransparencyAttrib
 from space_flight import DATAFILES_PATH
 
 MIN_DUST_ALPHA = 0.2
+MAX_DUST_ALPHA = 0.8
 
 
 class SpeedDustCloud:
+    """
+    A class to make a cloud of dust around the player
+    to get them a feeling of their ship's speed
+
+    Dust particles are sprites zooming past the player's ship
+    """
+
     def __init__(
         self,
         game,
-        num_particles: int = 300,
-        spread: int = 30,
+        num_particles: int = 100,
+        spread: float = 30,
         depth: float = 100.0,
         colors: List = ["white"],
     ):
@@ -30,29 +38,38 @@ class SpeedDustCloud:
         cm = CardMaker("particle")
         cm.setFrame(-0.02, 0.02, -0.02, 0.02)
 
-        root = NodePath("speedDust")
-        root.reparentTo(self.game.app.camera)
-        root.setTransparency(TransparencyAttrib.MAlpha)
-        root.setLightOff()
+        # Create a dummy node at the player's ship location to attach the dust
+        self.root = NodePath("speedDust")
+        self.root.reparentTo(self.game.player.ship.node)
+        self.root.setTransparency(TransparencyAttrib.MAlpha)
+        # Make that dust independent from scene lighting
+        self.root.setLightOff()
 
+        # Get the dust's colors
         self.colors = colors
 
+        # Initialize dust particles
         for _ in range(num_particles):
-            particle = root.attachNewNode(cm.generate())
+            particle = self.root.attachNewNode(cm.generate())
             particle.setBillboardPointEye()
             self._init_particle(particle)
             self.particles.append(particle)
 
-        self.root = root
         self.max_speed = self.game.player.ship.max_speed_mps
 
+        # Add the update task to the game's methods
         self.game.actor_methods[self.id] = [self.dust_update]
 
     def _init_particle(self, particle):
+        """
+        Initializes a particle with random color, scale and position
+
+        :param particle: A panda3d card object to hold the particle
+        """
         x = random.uniform(-self.spread, self.spread)
         z = random.uniform(-self.spread, self.spread)
         y = random.uniform(0, self.depth)
-        scaling = random.uniform(1.0, 4.0)
+        scaling = random.uniform(0.5, 2.0)
         color = random.choice(self.colors)
         particle.setPos(x, y, z)
         particle.setTexture(
@@ -64,12 +81,26 @@ class SpeedDustCloud:
         particle.setScale(scaling, scaling, scaling)
 
     def _reset_particle(self, particle):
-        particle.setY(particle.getY() + self.depth)
+        """
+        Resets a particle upstream of the player, at a random transversal location
+
+        :param particle: A panda3d card object to hold the particle
+        """
+        x = random.uniform(-self.spread, self.spread)
+        z = random.uniform(-self.spread, self.spread)
+        y = particle.getY() + self.depth
+        particle.setPos(x, y, z)
 
     def dust_update(self):
+        """
+        Updates the position of all particles and resets them if need be.
+        The dust's opacity increases with player speed to reinforce the feeling
+        """
         dt = self.game.game_time.get_time_step()
         speed = np.linalg.norm(self.game.player.ship.speed)
-        alpha = MIN_DUST_ALPHA + speed * (1.0 - MIN_DUST_ALPHA) / self.max_speed
+        alpha = (
+            MIN_DUST_ALPHA + speed * (MAX_DUST_ALPHA - MIN_DUST_ALPHA) / self.max_speed
+        )
         self.root.setAlphaScale(alpha)
         for particle in self.particles:
             particle.setY(particle.getY() - speed * dt)
