80177cc5da44aba798e454c4c827a868f3f8b978
Joe Dalton <guilhem.lavabre@laposte.net>
2026-05-14 12:47:47 +0200

    Wider error handling

--- scripts/launcher.py
+++ scripts/launcher.py
@@ -4,10 +4,6 @@ simulator = SpaceFlightSimulator()
 try:
     simulator.run()
 except Exception as e:
-    if "panda/src/pgraph/transformState.cxx" in str(e):
-        game = simulator.state_manager.get_current()
-        game.record.save()
-        simulator.state_manager.clear()
-        simulator.state_manager.replace(simulator.state_manager.MAIN_MENU_STATE)
-    else:
-        raise e
+    game = simulator.state_manager.get_current()
+    game.record.save()
+    raise e
